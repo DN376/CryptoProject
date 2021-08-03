2fc4d88164d04560e359a7a09a3d3f802143fc9c
Dave <dave030706@gmail.com>
2021-08-03 12:17:03 -0400

    formatted price and market cap

--- GetAPIData.py
+++ GetAPIData.py
@@ -37,7 +37,7 @@ def init():
     updateTimer = threading.Timer(60.0, update, [index+1])
     updateTimer.start()
     print (index)
-    if index >= 3:
+    if index >= 5:
       print("cancelled thread")
       updateTimer.cancel()
     
@@ -69,6 +69,27 @@ def init():
 
 init()
 
+@app.template_filter()
+def currencyFormat(value):
+    v = float(value)
+    return "${:,.2f} CAD".format(value)
+
+@app.template_filter()
+def marketCapFormat(value):
+    v = float(value)
+    lenVal = len("{:,.0f}".format(value))
+    if lenVal >= 15: #trillions
+      return "${:,.2f}T CAD".format(value/100_000_000_000)
+    if lenVal > 9: #billions
+      return "${:,.2f}B CAD".format(value/100_000_000)
+    if lenVal > 6: #millions
+      return "${:,.2f}M CAD".format(value/100_000)
+    if lenVal > 3: #thousands
+      val = math.floor(v/100)*100 
+      return "${:,.2f}K CAD".format(value/100)
+    return "{:,.2f}".format(value)
+app.add_template_filter(currencyFormat)
+app.add_template_filter(marketCapFormat)
 @app.route('/')
 @app.route('/coins')
 def coinsHome(): 
